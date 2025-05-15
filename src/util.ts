53e479bb363eb16b79c66a66086b57bbfb7bbc49
Markus Peitl <office@markuspeitl.com>
2025-05-15 23:50:28 +0200

    Fixed circular shift

--- src/util.ts
+++ src/util.ts
@@ -7,12 +7,12 @@ export function getModuloIndex(index: number, rangeLength: number) {
 	return rangeLength + indexInRange;
 
 	/*if (index < 0) {
-            const residue: number = Math.abs(index) % rangeLength;
-            return rangeLength - residue;
-        } else if (index >= rangeLength) {
-            return index % rangeLength;
-        }
-        return index;*/
+			const residue: number = Math.abs(index) % rangeLength;
+			return rangeLength - residue;
+		} else if (index >= rangeLength) {
+			return index % rangeLength;
+		}
+		return index;*/
 }
 
 // [1, 2, 3, 4, 5, 6] --> amount: 2 --> [6, 5, 1, 2, 3, 4]
@@ -25,12 +25,12 @@ function circularShiftRightInternal<ItemType>(array: ItemType[], amount: number,
 
 		var circShiftedArrayIndex: number = getModuloIndex(arrayPointerIndex, array.length);
 		//console.log(`in: ${arrayPointerIndex} modulo: ${circShiftedArrayIndex} of ${array.length} w: ${array}`);
-		return array[circShiftedArrayIndex];
+		return array[ circShiftedArrayIndex ];
 	});
 }
 const defaultCircShiftMode: '>>' | 'right' | '<<' | 'left' = '>>';
 export function circularShift<ItemType>(array: ItemType[], mode: '>>' | 'right' | '<<' | 'left' = defaultCircShiftMode, amount: number) {
-	if (mode === '>>' || 'right') {
+	if (mode === '>>' || mode === 'right') {
 		return circularShiftRightInternal(array, amount, false);
 	} else if (mode === '<<' || mode === 'left') {
 		return circularShiftRightInternal(array, amount, false);
@@ -40,5 +40,5 @@ export function circularShift<ItemType>(array: ItemType[], mode: '>>' | 'right' 
 
 export function getSymbolFromIndex<SymbolType>(index: number, symbolSet: SymbolType[]): SymbolType {
 	const shiftedIndex = getModuloIndex(index, symbolSet.length);
-	return symbolSet[shiftedIndex];
+	return symbolSet[ shiftedIndex ];
 }
